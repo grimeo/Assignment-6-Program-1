1d6f3fbf70a6733ca7f84b57829ef5a105ac9c19
Romeo <kilazlove@gmail.com>
2021-11-26 21:23:01 +0800

    create a function that accepts inputs from user

--- index.py
+++ index.py
@@ -4,3 +4,15 @@
 # Create a program that ask 4 numbers. 
 # Print the 4 numbers from highest to lowest using only if-else statement.
 
+# input
+# sort
+# print
+
+def getInput():
+    global n1,n2,n3,n4
+    n1 = int(input("Enter the 1st number: "))
+    n2 = int(input("Enter the 2nd number: "))
+    n3 = int(input("Enter the 3rd number: "))
+    n4 = int(input("Enter the 4th number: "))
+
+getInput()
